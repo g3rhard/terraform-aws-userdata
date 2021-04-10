b6d731262df44a304e0bfb2f4be9e8f4c103fda4
Kiriru Trofimov <kiriru.trofimov@gmail.com>
2021-04-10 17:17:17 +0200

    add noninteractive for apt

--- base.sh
+++ base.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Base
+export DEBIAN_FRONTEND=noninteractive
 sudo apt-get update
 sudo apt-get -qq install git-core software-properties-common python python-setuptools -y
 sudo apt-add-repository -y ppa:ansible/ansible
