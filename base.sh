598c66ed03b890b5988325469efc01526555f3ff
Kiriru Trofimov <kiriru.trofimov@gmail.com>
2021-04-10 17:40:25 +0200

    add upgrade step in base.sh

--- base.sh
+++ base.sh
@@ -2,6 +2,7 @@
 
 # Base
 sudo DEBIAN_FRONTEND=noninteractive apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
 sudo DEBIAN_FRONTEND=noninteractive apt-get -qq install git-core software-properties-common python python-setuptools -y
 sudo DEBIAN_FRONTEND=noninteractive apt-add-repository -y ppa:ansible/ansible
 sudo DEBIAN_FRONTEND=noninteractive apt-get update
