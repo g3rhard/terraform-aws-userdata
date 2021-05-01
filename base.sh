7808234fecb31ab4228fc141f39c8a7f0e410d2d
Kiriru Trofimov <kiriru.trofimov@gmail.com>
2021-05-01 19:21:09 +0200

    delete base.sh (move to g3rhard/aws-packer)

--- base.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Base
-sudo DEBIAN_FRONTEND=noninteractive apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
-sudo DEBIAN_FRONTEND=noninteractive apt-get -qq install git-core software-properties-common python python-setuptools -y
-sudo DEBIAN_FRONTEND=noninteractive apt-add-repository -y ppa:ansible/ansible
-sudo DEBIAN_FRONTEND=noninteractive apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get -qq install -y ansible
